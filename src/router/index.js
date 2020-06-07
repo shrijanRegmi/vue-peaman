eae7d192743498913b3427691a0c9580cceba47e
Shrijan Regmi <ilyyhs9@gmail.com>
2020-06-07 22:06:23 +0545

    firebase-added

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,26 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Auth from '../views/Auth.vue';
-import Home from '../views/Home.vue';
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Auth from "../views/Auth.vue";
+import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Auth',
-    component: Auth,
+    path: "/",
+    name: "Auth",
+    component: Auth
   },
   {
-    path: '/home',
-    name: 'Home',
-    component: Home,
-  },
+    path: "/home",
+    name: "Home",
+    component: Home
+  }
 ];
 
 const router = new VueRouter({
   routes,
+  mode: "history"
 });
 
 export default router;
